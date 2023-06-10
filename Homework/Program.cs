c5df27dc12c51eaf6f2482a8248f2a116f32fe2f
Ivan <ivansit357@gmail.com>
2023-06-10 16:56:17 +1000

    I do homework

--- Homework/Program.cs
+++ Homework/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Введите 2 числа: ");
+int number1 = int.Parse(Console.ReadLine()!);
+int number2 = int.Parse(Console.ReadLine()!);
+
+number1 = max;
+
+if (number2 > number1){
+    max = number2;
+}
+else
+{
+    max = number1;
+}
